f61927a41cb6d841f190f133020d58cd9acc6b95
ShangbChen <48976363+ShabChen@users.noreply.github.com>
2025-01-16 15:21:48 +0800

    Create vueuse.js

---
+++ vueuse.js
@@ -0,0 +1,14 @@
+import { computed } from 'vue'
+
+export function useComputed (fn) {
+  const map = new Map()
+  return function (...args) {
+    const key = JSON.stringify(args)
+    if (map.has(key)) {
+      return map.get(key)
+    }
+    const result = computed(() => fn(...args))
+    map.set(key, result)
+    return result
+  }
+}
